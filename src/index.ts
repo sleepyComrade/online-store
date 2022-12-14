f04dec742ecaa469bb5f7eb38abde7b00b8bbfb0
Leonid Khitskov <ivanovichivanivanov@protonmail.ch>
2022-12-14 09:12:34 +0900

    feat: create basic structure

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import './scss/style.scss';
+import  React  from "react";
+import * as ReactDOM from "react-dom/client";
+import { App } from "./reactapp";
+
+const rootDiv = document.getElementById('root');
+if (rootDiv === null || !(rootDiv instanceof HTMLElement)) {
+  throw new Error("error");
+}
+const root = ReactDOM.createRoot(rootDiv);
+root.render(App());
