107c0de91f9ab648a72b84f90cc1961637a954cc
Leonid Khitskov <ivanovichivanivanov@protonmail.ch>
2022-12-16 08:40:54 +0900

    feat: implement select component in cards navigation block

--- src/components/cards-navigation/navigation.tsx
+++ src/components/cards-navigation/navigation.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Found } from "../cards-navigation/found";
 import { ViewSwitcher } from "../cards-navigation/view-switcher";
+import { Sort } from "../cards-navigation/sort";
 
 export function CardsNav() {
   return (
     <div className="cards-nav">
+      <Sort></Sort>
       <Found></Found>
       <ViewSwitcher></ViewSwitcher>
     </div>
