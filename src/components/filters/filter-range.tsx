7437f724a722883a9d031292311fbb4223d50850
Leonid Khitskov <ivanovichivanivanov@protonmail.ch>
2022-12-29 13:11:47 +0900

    feat: make range sliders to work correct

--- src/components/filters/filter-range.tsx
+++ src/components/filters/filter-range.tsx
@@ -1,39 +1,48 @@
-import { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export default function FilterRange() {
-  const [minValue, setMinValue] = useState(2500);
-  const [maxValue, setMaxValue] = useState(7500);
-  const maxLimit = 10000;
-  const minLimit = 2000;
+  const [minValue, setMinValue] = useState(0);
+  const [maxValue, setMaxValue] = useState(2000);
+  const maxLimit = 2000;
+  const minLimit = 0;
+
+  const left = (minValue - minLimit) / (maxLimit - minLimit) * 100 + '%';
+  const right = (1 - (maxValue - minLimit) / (maxLimit - minLimit)) * 100 + '%';
+  const leftReverse = (maxValue - minLimit) / (maxLimit - minLimit) * 100 + '%';
+  const rightReverse = (1 - (minValue - minLimit) / (maxLimit - minLimit)) * 100 + '%';
 
   return (
     <div className="filter-range">
       <div className="filter-range__values">
         <div className="filter-range__field">
           <span>Min</span>
-          <span className="filter-range__value filter-range__value--min">{minLimit}</span>
+          <span className="filter-range__value filter-range__value--min">{minValue > maxValue ? maxValue : minValue}</span>
         </div>
 
         <div className="filter-range__separator"> - </div>
 
         <div className="filter-range__field">
           <span>Max</span>
-          <span className="filter-range__value filter-range__value--max">{maxLimit}</span>
+          <span className="filter-range__value filter-range__value--max">{maxValue < minValue ? minValue : maxValue}</span>
         </div>
       </div>
 
       <div className="filter-range__slider">
         <div className="filter-range__progress" style={{
-          left: (minValue - minLimit) / (maxLimit - minLimit) * 100 + '%',
-          right: (1 - (maxValue - minLimit) / (maxLimit - minLimit)) * 100 + '%'
+          left: minValue > maxValue ? leftReverse : left,
+          right: minValue > maxValue ? rightReverse : right
         }}></div>
       </div>
 
       <div className="filter-range__container">
         <input type="range" className="filter-range__input filter-range__input--min" min={minLimit} max={maxLimit} value={minValue}
-          onChange={(e) => { setMinValue(e.target.valueAsNumber) }} />
+          onChange={(e) => {
+            setMinValue(+e.target.value);
+          }} />
         <input type="range" className="filter-range__input filter-range__input--max" min={minLimit} max={maxLimit} value={maxValue}
-          onChange={(e) => { setMaxValue(e.target.valueAsNumber) }} />
+          onChange={(e) => {
+            setMaxValue(+e.target.value);
+          }} />
       </div>
     </div>
   )
