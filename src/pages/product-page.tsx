0d2b4cbb0e48fed1cfeb18e3bc5f1d4e1170d4e3
Ramitsan <ramitsan@gmail.com>
2023-01-05 20:24:01 +0300

    feat: add main styles for product page

--- src/pages/product-page.tsx
+++ src/pages/product-page.tsx
@@ -24,43 +24,43 @@ export default function ProductPage({ products, isModal, setIsModal }: ProductPa
   const btnColor = product.counter > 0 ? 'crimson' : '#48647f';
 
   return (
-      <main>
-        <div className="main-container main-container--product-page">
-          <section className="product">
-            <div className="product__breadcrumbs">
-              <ul className="product__breadcrumbs-list">
-                <li className="product__breadcrumbs-item"><span>Store &gt;&gt;</span></li>
-                <li className="product__breadcrumbs-item"><span>{category} &gt;&gt;</span></li>
-                <li className="product__breadcrumbs-item"><span>{brand} &gt;&gt;</span></li>
-                <li className="product__breadcrumbs-item"><span>{title} &gt;&gt;</span></li>
+    <main>
+      <div className="main-container main-container--product-page">
+        <div className="product__breadcrumbs">
+          <ul className="product__breadcrumbs-list">
+            <li className="product__breadcrumbs-item"><span>Store</span>&gt;&gt;</li>
+            <li className="product__breadcrumbs-item"><span>{category}</span>&gt;&gt;</li>
+            <li className="product__breadcrumbs-item"><span>{brand}</span>&gt;&gt;</li>
+            <li className="product__breadcrumbs-item"><span>{title}</span></li>
+          </ul>
+        </div>
+        <section className="product">
+          <div className="product__header">
+            <h2 className="product__title">{title}</h2></div>
+          <div className="product__info">
+            <div className="product__images"></div>
+            <div className="product__full-info">
+              <ul className="product__full-info-list">
+                <li className="product__full-info-item"><p>Rating: <span>{rating}</span></p></li>
+                <li className="product__full-info-item"><p>Category: <span>{category}</span></p></li>
+                <li className="product__full-info-item"><p>Brand: <span>{brand}</span></p></li>
+                <li className="product__full-info-item"><p>Description: <span>{description}</span></p></li>
+                <li className="product__full-info-item"><p>Discount Percentage: <span>{discountPercentage}</span></p></li>
+                <li className="product__full-info-item"><p>Stock: <span>{stock}</span></p></li>
               </ul>
             </div>
-            <div className="product__header">
-              <h2 className="product__title">{title}</h2></div>
-            <div className="product__info">
-              <div className="product__images"></div>
-              <div className="product__full-info">
-                <ul className="product__full-info-list">
-                  <li className="product__full-info-item"><p>Description: <span>{description}</span></p></li>
-                  <li className="product__full-info-item"><p>Category: <span>{category}</span></p></li>
-                  <li className="product__full-info-item"><p>Brand: <span>{brand}</span></p></li>
-                  <li className="product__full-info-item"><p>Discount Percentage: <span>{discountPercentage}</span></p></li>
-                  <li className="product__full-info-item"><p>Rating: <span>{rating}</span></p></li>
-                  <li className="product__full-info-item"><p>Stock: <span>{stock}</span></p></li>
-                </ul>
-              </div>
-              <div className="prodict__buy-block">
-                <p className="product__price">${new Intl.NumberFormat("en").format(price)}</p>
-                <button style={{ background: `${btnColor}` }}>        
-                </button>
-                <button className="btn promo-block__button promo-block__button--buy" onClick={() => setIsModal(true)}>Buy now</button>
-              </div>
+            <div className="prodict__buy-block">
+              <p className="product__price">${new Intl.NumberFormat("en").format(price)}</p>
+              <button className="btn product__button product__button--add" style={{ background: `${btnColor}` }}>
+              </button>
+              <button className="btn promo-block__button promo-block__button--buy" onClick={() => setIsModal(true)}>Buy now</button>
             </div>
-          </section>
-          <Modal open={isModal} setState={setIsModal}>
-            <PaymentForm setState={setIsModal} />
-          </Modal>
-        </div>
-      </main>
+          </div>
+        </section>
+        <Modal open={isModal} setState={setIsModal}>
+          <PaymentForm setState={setIsModal} />
+        </Modal>
+      </div>
+    </main>
   );
 }
