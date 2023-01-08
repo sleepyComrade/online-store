d071ea01a9f32718a3a63c64f5c2473cea749d2d
Ramitsan <ramitsan@gmail.com>
2023-01-08 19:44:51 +0300

    feat: add removal of identical pictures

--- src/components/images-gallery/images-gallery.tsx
+++ src/components/images-gallery/images-gallery.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type ImageItemProps = {
   src: string;
@@ -18,13 +18,23 @@ type ImagesGalleryProps = {
 }
 
 export default function ImagesGallery({ images}: ImagesGalleryProps) {
+  const [imgs, setImgs] = useState<Array<Blob>>([]);
+
   const [selectedImage, setSelectedImage] = useState(images[0]);
   const onPreviewClick = (image: string) => {setSelectedImage(image)}
+  useEffect(() => {
+    Promise.allSettled(images.map(src => fetch(src).then(res => res.blob()))).then(res => {
+      const fulfilled = res.filter(it => it.status == "fulfilled") as Array<PromiseFulfilledResult<Blob>>;
+      const values = fulfilled.map(it => it.value).sort((a, b) => a.size - b.size);
+      const filtered = values.filter((it, index, arr) => it.size !== arr[index + 1]?.size);
+      setImgs(filtered);
+    });
+  }, []);
 
   return (
     <div className="image-gallery__wrapper">
       <div className="image-gallery__previews">
-        {images.map((src) => <ImageItem key={src} src={src} onPreviewClick={onPreviewClick} />)}
+        {imgs.map(img => <ImageItem key={img.size} src={URL.createObjectURL(img)} onPreviewClick={onPreviewClick} />)}
       </div>
       <div className="image-gallery__big-image-wrapper">
         <img className="image-gallery__big-image" src={selectedImage} />
